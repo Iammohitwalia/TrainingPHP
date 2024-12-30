846bef029075b51b8c8ea45d1bdb24a702a58195
Rajanii-Thakur <rajanithakur416@gmail.com>
2024-12-30 11:44:49 +0530

    new files

--- operators.php
+++ operators.php
@@ -56,5 +56,119 @@ $y = -60;
 echo $x ** $y ."<br>";
 $x = 500;
 $y  =8900;
-echo $x ** $y;
+echo $x ** $y . "<br>" . "<br>";
+
+//Comparision operators
+//Equal 
+$x = 300;
+$y = 300;
+var_dump ($x == $y);
+$x = 800;
+$y = 500;
+var_dump ($x == $y);
+
+//identical operators
+$x = 600;
+$y = 200;
+var_dump($x === $y);
+
+$x = "300";
+$y = "306";
+var_dump($x === $y);
+
+$x = 410;
+$y = "206";
+var_dump($x === $y);
+
+
+//not equal to
+
+$x = 20;
+$y = 30;
+var_dump($x != $y);
+
+$x = 30;
+$y = 30;
+var_dump($x != $y);
+
+//Not equal to
+
+$x = 20;
+$y = 20;
+var_dump($x <> $y);
+
+$x =500;
+$y = 600;
+var_dump($x <> $y);
+
+//Not identical 
+
+$x = 300;
+$y = "300";
+var_dump($x !== $y);
+
+$x = 300;
+$y = 200;
+var_dump($x !== $y);
+
+//Greater than
+
+$x = 700;
+$y = 500;
+var_dump($x > $y);
+
+$x = 200;
+$y = 500;
+var_dump($x > $y);
+
+//less than
+
+$x = 400;
+$y = 500;
+var_dump($x < $y);
+
+$x = 600;
+$y = 500;
+var_dump($x < $y);
+
+//Greater than equal to
+
+$x = 650;
+$y = 600;
+var_dump($x >= $y);
+
+$x = 600;
+$y = 600;
+var_dump($x >= $y);
+
+$x = 500;
+$y = 600;
+var_dump($x >= $y);
+
+//Less than wqual to
+
+$x = 600;
+$y = 900;
+var_dump($x <= $y);
+
+$x = 200;
+$y = 200;
+var_dump($x <= $y);
+
+$x = 20;
+$y = 10;
+var_dump($x <= $y);
+
+//Spaceship
+
+$x =700;
+$y = 600;
+var_dump($x <=> $y);
+
+$x =500;
+$y = 600;
+var_dump($x <=> $y);
+$x =220;
+$y = 220;
+var_dump($x <=> $y);
 ?>
---
+++ statements.php
@@ -0,0 +1,82 @@
+<?php
+
+//if statement
+$x =  22;
+$y = 19;
+ 
+if ($x > $y){
+echo "How are you? <br>";
+}
+$x =70;
+$y =90;
+if($x > $y){
+    echo "what are you doing?<br>";
+}
+
+$x =70;
+$y =80;
+if($x = $y){
+    echo "what are you doing?<br>";
+}
+
+// if...else
+
+$x = 400;
+$y = 600;
+     
+if ($x < $y){
+    echo "Good to see you<br>";
+}
+else{
+    echo "Good Morning <br>";
+}
+if (100 > 300){
+    echo "Good Evening";
+}
+else {
+    echo"Hello kitty <br>";
+}
+if (500<=400){
+    echo "Hello Everyone";
+ }
+ else
+ {
+    echo"Oh wow thats great<br>";
+ }
+    
+//if...elseif...else
+
+if(200<100)
+{
+    echo "Heloo <br>";
+}
+elseif(200>500){
+    echo "Hello world";
+}
+ else {
+ echo "Hello pretty girl <br>";
+ }
+
+ if(200>100)
+{
+    echo "Heloo <br>";
+}
+elseif(200>500){
+    echo "Hello world";
+}
+ else {
+ echo "Hello pretty girl <br>";
+ }
+
+ if(200<100)
+{
+    echo "Heloo ";
+}
+elseif(200<500){
+    echo "Thanks for your support";
+}
+ else {
+ echo "Hello pretty girl";
+ }
+
+?>
