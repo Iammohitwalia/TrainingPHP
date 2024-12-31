7d3b5c8557773cb05b672dba18956b5fc04494a7
Rajanii-Thakur <rajanithakur416@gmail.com>
2024-12-31 14:29:15 +0530

    new

---
+++ array.php
@@ -0,0 +1,41 @@
+<?php
+
+//Indexed Array
+
+$color = array("Red","Blue","Green","Black");
+echo $color [0];
+
+$car = array("Alto","BMW","Audi","Thar","Scorpio");
+echo $car[3];
+
+$fruits = array("Apple","Kiwi","Pinapple","Mango");
+echo $fruits[2];
+
+
+//Associative Array
+
+$person = ["name"=> "Cherry" ,"age" => "28", "city" => "Newyork"];
+echo $person["city"];
+
+$car = array("brand" => "Ford", "model" => "Mustang", "year" => 1964 , "city" => "canada");
+echo $car ["model"];
+
+
+//Multidimensional Array
+
+$student = array(
+   array ("John", 25,"A"),
+   array("Jane", 30,"B"),
+   array("Doe", 35, "C"),
+);
+echo $student [1][0];
+
+$students =array(
+    array("Rohan",10,"A","English"),
+    array("Mohan",11 ,"B","Hindi"),
+    array("Ram",12,"C","Science"),
+    array("Shyam",13,"D","Sanskrit"),
+);
+echo $students [3][3];
+
+?>
