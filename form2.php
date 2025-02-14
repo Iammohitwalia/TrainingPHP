593d46cfb726e0faf76542a960ff8dcb0d57f512
Rajanii-Thakur <rajanithakur416@gmail.com>
2025-02-14 13:58:23 +0530

    changes

--- form2.php
+++ form2.php
@@ -2,18 +2,18 @@
 $name = $email = $message = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //  data from form
-    $name = htmlspecialchars($_POST["name"]);
-    $lname = htmlspecialchars($_POST["last-name"]);
-    $email = htmlspecialchars($_POST["email"]);
-$address = htmlspecialchars($_POST["address"]);
+    $name = ($_POST["name"]);
+    $lname = ($_POST["last-name"]);
+    $email = ($_POST["email"]);
+$address = ($_POST["address"]);
 }
 ?>
-<form action="" method="POST">
+<form action="" method="POST" target="_self">
   <input type="text" placeholder="First Name" name="name" id="name"><br><br>
   <input type="text" placeholder="Last Name" name="last-name" id="name" ><br><br>
 <input type="email" placeholder="Email" name="email" id="email"><br><br>
 <input type="text" placeholder="Address" name="address" id="address"><br><br>
-<button type="button" onclick="alert('Form Submitted Successfully')">Submit</button>
+<input type="submit">
 
 </form>
 <?php
@@ -24,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo  $lname . "<br>";
     echo  $email . "<br>";
     echo  $address ;
+    
   
 }
 ?>
