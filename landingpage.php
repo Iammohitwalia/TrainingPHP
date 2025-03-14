69eeac9a98a90d91df025bd7e5711c4983566a3b
Rajanii-Thakur <rajanithakur416@gmail.com>
2025-03-14 12:39:20 +0530

    Update landingpage.php

--- landingpage.php
+++ landingpage.php
@@ -33,21 +33,24 @@
       </div>
     </div>
   </nav>
-  <div class="container">
-    <div class="row">
-      <div class="p-3">
-        <section>
-          <div class="px-4 py-5 my-5 text-center">
-            <div class="col-lg-6 mx-auto">
-              <h3 class="display-5 text-body-emphasis text-success-emphasis">Hero</h3>
-              <p class="lead mb-4 text-primary-emphasis">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                <a href="#" class="btn btn-primary mt-3">Primary Button </a>
-                <a href="#" class="btn btn-outline-secondary mt-3">Secondary Button </a>
-              </div>
-            </div>
-          </div>
-        </section>
+  <section>
+  <div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="../img/img.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Centered Hero</h1>
+        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+  <div class="b-example-divider"></div>
         <section>
           <div class="px-4 py-5 my-5 text-center">
             <div class="col-lg-6 mx-auto">
